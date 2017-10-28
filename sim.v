20d3327135ea57b78c2611859fe3f3b1fd3a5a78
Miodrag Milanovic <mmicko@gmail.com>
2017-10-28 14:02:05 +0200

    fix simulation

--- sim.v
+++ sim.v
@@ -1,10 +1,11 @@
+`timescale 1ns/1ps 
 module sim();
 	reg clk = 0;
 	reg reset;
 	wire [7:0] display_out;
 	wire hlt;
 
-	grom_computer cpu(.clk(clk),.reset(reset),.hlt(hlt),.display_out(display_out));
+	grom_computer computer(.clk(clk),.reset(reset),.hlt(hlt),.display_out(display_out));
 
 	always
 		#(5) clk <= !clk;
@@ -16,15 +17,7 @@ module sim();
 		reset = 1;
 		#20
 		reset = 0;
-
-		#300
-		#300
-		#300
-		reset = 1;
-		#20
-		reset = 0;
-		#300
-		#300
+		#900
 		$finish;
 	end
 endmodule
